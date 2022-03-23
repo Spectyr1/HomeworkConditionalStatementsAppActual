a0651c564a955d8995b01ba16780659a2300a3d8
Danny Warren <danny@theforbiddenarts.com>
2022-03-23 16:00:15 -0400

    submit code

--- HomeworkConditionalStatements/Program.cs
+++ HomeworkConditionalStatements/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string name;
+
+Console.Write("hello, please enter your first name: ");
+name = Console.ReadLine();
+
+if (name.ToLower() == "tim")
+{
+    Console.WriteLine("Hello professor.");
+}
+else
+{
+    Console.WriteLine("Hello student");
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 HomeworkConditionalStatementsAppActual
+
+Create a Console Application that asks the user for thier name.
+Welcome me (Tim) as professor or anyone else as student.
+Make sure that "TIM" also gets called professor.
+
+
+// get name from user
+// welcome "tim" as professor
+// welcome others as student
